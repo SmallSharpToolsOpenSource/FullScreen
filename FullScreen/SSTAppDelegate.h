9aa42f630f4ec32b734e242162698bdc1154ea12
Brennan Stehling <brennan@smallsharptools.com>
2014-05-25 15:37:11 -0500

    Initial Commit

---
+++ FullScreen/SSTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SSTAppDelegate.h
+//  FullScreen
+//
+//  Created by Brennan Stehling on 5/25/14.
+//  Copyright (c) 2014 SmallSharpTools. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SSTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FullScreen/SSTViewController.m
@@ -0,0 +1,29 @@
+//
+//  SSTViewController.m
+//  FullScreen
+//
+//  Created by Brennan Stehling on 5/25/14.
+//  Copyright (c) 2014 SmallSharpTools. All rights reserved.
+//
+
+#import "SSTViewController.h"
+
+@interface SSTViewController ()
+
+@end
+
+@implementation SSTViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

66f305f7b2bab6e94bf559d675b9d61e1fabd0c1
Brennan Stehling <brennan@smallsharptools.com>
2014-05-26 17:01:42 -0500

    first commit

---
+++ FullScreen/SSTBaseNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  SSTBaseNavigationController.h
+//  FullScreen
+//
+//  Created by Brennan Stehling on 5/25/14.
+//  Copyright (c) 2014 SmallSharpTools. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SSTBaseNavigationController : UINavigationController
+
+@end
